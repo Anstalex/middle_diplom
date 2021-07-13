ee7a045a59efd7968a25a036b554d54189a7f5b3
Anstalex <a.v.bakina@mail.ru>
2021-07-13 13:57:27 +0600

    feat(scope): add open/close service-popup
    #popup

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,15 +2,23 @@ import handler from "./handler";
 
 const toggleModal = () => {
     const popup = document.querySelector('.header-modal--opened');
+    const servicePopup = document.querySelector('.services-modal--opened');
     const overlay = document.querySelector('.overlay');
     handler(document, 'click', (e) => {
         const target = e.target;
         if (target.closest('.btn-warning')){
             overlay.classList.remove('overlay-close');
             popup.classList.remove('header-modal');
-        }else if(!target.closest('.header-modal--opened')||(target.matches('.header-modal__close'))){
+        }else if(target.closest('.btn-success')){
+            overlay.classList.remove('overlay-close');
+            servicePopup.classList.remove('services-modal');
+        }else if(!target.closest('.header-modal--opened')&&
+            (!target.closest('.services-modal--opened'))||
+            (target.matches('.header-modal__close'))||
+            (target.matches('.services-modal__close'))){
             overlay.classList.add('overlay-close');
             popup.classList.add('header-modal');
+            servicePopup.classList.add('services-modal');
         }
 
     })
