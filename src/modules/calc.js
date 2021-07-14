a577982b18b894c1d6bacdb474bcc03ebf6278ba
Anstalex <a.v.bakina@mail.ru>
2021-07-15 00:04:55 +0600

    feat(scope): add calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,3 +1,20 @@
+import handler from "./handler";
+
 const calc = () => {
-};
+    const inputsCalc = document.getElementById('calc-input');
+    const calcType = document.getElementById('calc-type');
+    const calcMaterial = document.getElementById('calc-type-material');
+    const calcTotal = document.getElementById('calc-total');
+    const arrInputs =document.querySelectorAll('.calc-control');
+arrInputs.forEach((item)=>{
+    handler(item, 'input', () => {
+        inputsCalc.value = inputsCalc.value.replace(/\D/g, '');
+        if ((!isNaN(+calcType.value)) && (!isNaN(+calcMaterial.value))) {
+            let res = inputsCalc.value * calcType.options[calcType.selectedIndex].value * calcMaterial.options[calcMaterial.selectedIndex].value;
+            calcTotal.value = Math.floor(res);
+        }
+    });
+})
+
+}
 export default calc;
